5e7ca87f2d9d0fe990a506dca482a8c28a2d430f
mateusz-git <215851@edu.p.lodz.pl>
2020-11-10 15:30:46 +0100

    Get content from php file

--- src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
+++ src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
@@ -4,9 +4,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 @RestController
 public class Controller {
     @GetMapping("/calendar")
-    public void getCalendar(@RequestParam int year,@RequestParam int month ){
+    public void getCalendar(@RequestParam int year, @RequestParam int month) throws IOException {
+        String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac" + month;
+        getCalendarFromWeeia(url);
+
+    }
+
+    private void getCalendarFromWeeia(String urlWeeia) throws IOException {
+        URL url = new URL(urlWeeia);
+        URLConnection connection = url.openConnection();
+        StringBuilder fromWebsite = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            fromWebsite.append(line);
+        }
+        String htmlInfo = fromWebsite.toString();
+        System.out.println(htmlInfo);
     }
 }
