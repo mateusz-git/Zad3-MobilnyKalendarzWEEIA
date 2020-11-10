cc26c273bfda535052703dabfa16cb6cadcc2ae5
mateusz-git <215851@edu.p.lodz.pl>
2020-11-10 14:51:57 +0100

    Created endpoint

--- src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
+++ src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
@@ -1,7 +1,12 @@
 package com.example.Mobilny.Kalendarz.WEEIA;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @GetMapping("/calendar")
+    public void getCalendar(@RequestParam int year,@RequestParam int month ){
+    }
 }
