08a5bc6b47ba0c197e15bbdafd554a3d63eea759
mateusz-git <215851@edu.p.lodz.pl>
2020-11-10 15:52:28 +0100

    Created class to keep event day

---
+++ src/main/java/com/example/Mobilny/Kalendarz/WEEIA/EventDay.java
@@ -0,0 +1,27 @@
+package com.example.Mobilny.Kalendarz.WEEIA;
+
+public class EventDay {
+    int day;
+    String description;
+
+    public EventDay(int day, String description) {
+        this.day = day;
+        this.description = description;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
