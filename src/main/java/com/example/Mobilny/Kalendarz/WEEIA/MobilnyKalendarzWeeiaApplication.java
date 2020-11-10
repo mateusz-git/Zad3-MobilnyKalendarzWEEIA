9e1150c8df7027aee63f2aff89fca337582f73e9
mateusz-git <215851@edu.p.lodz.pl>
2020-11-10 14:41:53 +0100

    Generated project

---
+++ src/main/java/com/example/Mobilny/Kalendarz/WEEIA/MobilnyKalendarzWeeiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mobilny.Kalendarz.WEEIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilnyKalendarzWeeiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilnyKalendarzWeeiaApplication.class, args);
+	}
+
+}
