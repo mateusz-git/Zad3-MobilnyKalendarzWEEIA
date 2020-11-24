fe02603f782d3daadb77422d7dd1434258311fa2
mateusz-git <215851@edu.p.lodz.pl>
2020-11-24 14:17:25 +0100

    Get events and days from selected document

--- src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
+++ src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
@@ -36,9 +36,10 @@ public class Controller {
         String htmlContent = fromWebsite.toString();
         Document document = Jsoup.parse(htmlContent);
         Elements activeElement = document.select("td.active");
-        for (Element element : activeElement) {
-            System.out.println(element);
-        }
+        Elements events = activeElement.select("div.InnerBox");
+        Elements days = activeElement.select("a.active");
 
+        System.out.println(events);
+        System.out.println(days);
     }
 }
