c1cadb870c62f24e3a6c61aa40537b5fd5e59e13
mateusz-git <215851@edu.p.lodz.pl>
2020-11-24 14:35:13 +0100

    Created ICalendar, set name and download ICS file with iCalendar

--- src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
+++ src/main/java/com/example/Mobilny/Kalendarz/WEEIA/Controller.java
@@ -1,22 +1,30 @@
 package com.example.Mobilny.Kalendarz.WEEIA;
 
+import biweekly.Biweekly;
+import biweekly.ICalendar;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Controller {
+    private final static String FILE_NAME = "Calendar_weeia.ics";
     @GetMapping("/calendar")
     public String getCalendar(@RequestParam int year, @RequestParam int month) throws IOException {
         if (year <= 1900 || year >= 2100)
@@ -28,8 +36,14 @@ public class Controller {
         if (month < 10)
             monthString = "0" + monthString;
 
+        ICalendar iCalendar = new ICalendar();
+        iCalendar.setExperimentalProperty("X-WR-CALNAME", "Kalendarz");
+
         String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + monthString;
-        System.out.println(getCalendarFromWeeia(url));
+
+        File file = new File(FILE_NAME);
+        Biweekly.write(iCalendar).go(file);
+
         return "";
     }
 
